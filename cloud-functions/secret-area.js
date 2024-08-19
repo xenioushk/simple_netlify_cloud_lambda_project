355a3b9cbdfc02cb13a13ad6b336d33de48de15c
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-08-19 16:58:59 +0300

    updated the password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -11,7 +11,7 @@ exports.handler = function (event, context, callback) {
     body = {}
   }
 
-  if (body.password == "javascipt") {
+  if (body.password == "javascript") {
     callback(null, {
       statusCode: 200,
       body: secretContent,
