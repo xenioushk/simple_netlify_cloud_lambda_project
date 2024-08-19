570976e2573591023a03fa938706457a5faff662
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-08-19 16:57:05 +0300

    updated the secret area function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the secret area of the simple netlify cloud lambda project.",
-  })
+  // Extract data.
+
+  let body
+  const secretContent = `<h2>Welcome to the admin area</h2>
+  <p>My username is: Himel</p>`
+
+  if (event.body) {
+    body = JSON.parse(event.body)
+  } else {
+    body = {}
+  }
+
+  if (body.password == "javascipt") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    })
+  } else {
+    callback(null, {
+      statusCode: 401,
+      body: "Incorrect password!",
+    })
+  }
 }
