2845a24be8043f308eb92fb646f1d94b7b7141f8
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-08-19 17:13:48 +0300

    connect the form with api

--- src/scripts/modules/SecretForm.js
+++ src/scripts/modules/SecretForm.js
@@ -1,16 +1,35 @@
+import axios from "axios"
+
 class SecretForm {
   constructor(parameters) {
     this.secretForm = document.querySelector(".secret_form")
+    this.cotentArea = document.querySelector(".content-area")
+    this.passwordField = document.querySelector("#password")
+    this.passwordField.focus()
     this.events()
   }
 
   events() {
-    this.secretForm.addEventListener("submit", this.handleFormSubmission.bind())
+    this.secretForm.addEventListener("submit", (e) => {
+      e.preventDefault()
+      this.handleFormSubmission()
+    })
   }
 
   handleFormSubmission(e) {
-    e.preventDefault()
-    alert("Submitted")
+    axios
+      .post("https://snclp.netlify.app/.netlify/functions/secret-area", {
+        password: this.passwordField.value,
+      })
+      .then((response) => {
+        this.secretForm.remove()
+        this.cotentArea.innerHTML = response.data
+      })
+      .catch((error) => {
+        this.cotentArea.innerHTML = `Invalid password!`
+        this.passwordField.value = ""
+        this.passwordField.focus()
+      })
   }
 }
 
